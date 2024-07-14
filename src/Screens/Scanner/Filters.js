3c819c32579864c0241b0e1654f1f97651d07a48
Huzaifa-Farooq <huzaifa.farooq.2003@gmail.com>
2024-07-14 09:57:00 +0500

    remove filters

--- src/Screens/Scanner/Filters.js
+++ src/Screens/Scanner/Filters.js
@@ -1,105 +1,105 @@
 import { Image } from 'react-native';
-import {
-    Sepia,
-    Grayscale,
-    Tint,
-    Invert,
-    Saturate,
-    Contrast,
-    Sharpen,
-    Brightness
-} from 'react-native-image-filter-kit';
+// import {
+//     Sepia,
+//     Grayscale,
+//     Tint,
+//     Invert,
+//     Saturate,
+//     Contrast,
+//     Sharpen,
+//     Brightness
+// } from 'react-native-image-filter-kit';
 
 
 
-export const GrayscaledImage = ({ imageSource, style, onExtractImage }) => {
-    return (
-        <Grayscale
-            onExtractImage={onExtractImage}
-            extractImageEnabled={onExtractImage ? true : onExtractImage}
-            image={<Image source={imageSource} style={style} />}
-        />
-    );
-}
+// export const GrayscaledImage = ({ imageSource, style, onExtractImage }) => {
+//     return (
+//         <Grayscale
+//             onExtractImage={onExtractImage}
+//             extractImageEnabled={onExtractImage ? true : onExtractImage}
+//             image={<Image source={imageSource} style={style} />}
+//         />
+//     );
+// }
 
 
 
-const CombinedFiltersImage = ({ imageSource, style, onExtractImage }) => (
-    <Sharpen
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        image={<Contrast
-            amount={2}
-            image={
-                <Saturate
-                    image={<Image source={imageSource} style={style} />}
-                    amount={2}
-                />
-            }
-        />}
-    />
-);
+// const CombinedFiltersImage = ({ imageSource, style, onExtractImage }) => (
+//     <Sharpen
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         image={<Contrast
+//             amount={2}
+//             image={
+//                 <Saturate
+//                     image={<Image source={imageSource} style={style} />}
+//                     amount={2}
+//                 />
+//             }
+//         />}
+//     />
+// );
 
-const InvertedImage = ({ imageSource, style, onExtractImage }) => (
-    <Invert
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        image={<Image source={imageSource} style={style} />}
-    />
-);
+// const InvertedImage = ({ imageSource, style, onExtractImage }) => (
+//     <Invert
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         image={<Image source={imageSource} style={style} />}
+//     />
+// );
 
-const SaturatedImage = ({ imageSource, style, onExtractImage }) => (
-    <Saturate
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        amount={2}
-        image={<Image source={imageSource} style={style} />}
-    />
-);
+// const SaturatedImage = ({ imageSource, style, onExtractImage }) => (
+//     <Saturate
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         amount={2}
+//         image={<Image source={imageSource} style={style} />}
+//     />
+// );
 
-const SepiaTintedImage = ({ imageSource, style, onExtractImage }) => (
-    <Sepia
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        amount={0.4}
-        image={
-            <Tint
-                amount={0.2}
-                image={<Image source={imageSource} style={style} />}
-            />
-        }
-    />
-);
+// const SepiaTintedImage = ({ imageSource, style, onExtractImage }) => (
+//     <Sepia
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         amount={0.4}
+//         image={
+//             <Tint
+//                 amount={0.2}
+//                 image={<Image source={imageSource} style={style} />}
+//             />
+//         }
+//     />
+// );
 
-const HighContrastImage = ({ imageSource, style, onExtractImage }) => (
-    <Contrast
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        amount={2}
-        image={<Image source={imageSource} style={style} />}
-    />
-);
+// const HighContrastImage = ({ imageSource, style, onExtractImage }) => (
+//     <Contrast
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         amount={2}
+//         image={<Image source={imageSource} style={style} />}
+//     />
+// );
 
-const sharpenImage = ({ imageSource, style, onExtractImage }) => (
-    <Sharpen
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        image={<Image source={imageSource} style={style} />}
-    />
-);
+// const sharpenImage = ({ imageSource, style, onExtractImage }) => (
+//     <Sharpen
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         image={<Image source={imageSource} style={style} />}
+//     />
+// );
 
 const defaultImage = ({ imageSource, style }) => (
     <Image source={imageSource} style={style} />
 );
 
-const lightenImage = ({ imageSource, style, onExtractImage }) => (
-    <Brightness
-        onExtractImage={onExtractImage}
-        extractImageEnabled={onExtractImage ? true : false}
-        amount={0.5}
-        image={<Image source={imageSource} style={style} />}
-    />
-);
+// const lightenImage = ({ imageSource, style, onExtractImage }) => (
+//     <Brightness
+//         onExtractImage={onExtractImage}
+//         extractImageEnabled={onExtractImage ? true : false}
+//         amount={0.5}
+//         image={<Image source={imageSource} style={style} />}
+//     />
+// );
 
 
 const Filters = {
